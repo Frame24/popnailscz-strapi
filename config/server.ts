f3c8c3c050c4705d8ba269566a89cf297193e3bd
Frame24 <hks9@yandex.ru>
2024-11-28 22:50:01 +0500

    Исправил ошибки редайректа на IP вместо домена

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: '0.0.0.0',
   port: env.int('PORT', 1337),
-  url: 'http://185.22.232.20:1337', // замените YOUR_SERVER_IP на IP-адрес вашего VPS
+  url: 'https://api.popnails.cz', // Убедитесь, что указан правильный URL
   app: {
     keys: env.array('APP_KEYS'),
   },
